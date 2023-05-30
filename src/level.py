912bd560a5b7f5ce2c066ead4cd82023c56395a6
Yago Goulu <yago.goulu@gmail.com>
2023-05-30 20:48:42 -0300

    second iteration on placeholder project

---
+++ src/level.py
@@ -0,0 +1,26 @@
+import pygame
+from settings import *
+from tile import *
+from player import *
+
+class Level:
+    def __init__(self):
+        
+        self.display_surface = pygame.display.get_surface()
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = row_index * TILESIZE
+                y = col_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    Player((x,y),[self.visible_sprites])
+
+    def run(self):
+        self.visible_sprites.draw(self.display_surface)
