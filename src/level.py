4d287bafed559e118544c215273b3b562948fcdc
Yago Goulu <yago.goulu@gmail.com>
2023-05-31 20:50:01 -0300

    movement and collision

--- src/level.py
+++ src/level.py
@@ -1,7 +1,8 @@
 import pygame
 from settings import *
-from tile import *
-from player import *
+from tile import Tile
+from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -20,7 +21,9 @@ class Level:
                 if col == 'x':
                     Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
                 if col == 'p':
-                    Player((x,y),[self.visible_sprites])
+                    self.player = Player((x,y),[self.visible_sprites], self.obstacles_sprites)
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
